592cff375683b22167b978d359c67ad8b048b2da
Anton <voron91131@gmail.com>
2025-05-29 23:39:12 +0300

    вынесены переменные окружения

--- payments_system/settings.py
+++ payments_system/settings.py
@@ -9,23 +9,27 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import environ
+import os
 from pathlib import Path
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    "django-insecure-)80on6d-q=g+)rs&lh^u08tu=o^vnk4dv=y#g1#7@x*3sie6-w"
-)
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["*"]
 
@@ -90,13 +94,13 @@ WSGI_APPLICATION = "payments_system.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": "payments_db",  # название твоей базы данных
-        "USER": "anton",  # имя пользователя в MySQL
-        "PASSWORD": "anton",  # пароль от MySQL
-        "HOST": "localhost",  # или IP MySQL-сервера
-        "PORT": "3306",  # стандартный порт MySQL
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
     }
 }
 
